da165121675cde900932b15181544eee2377f908
ielizarraz <iemonkey99@yahoo.com>
2021-09-20 19:53:05 -0700

    Cleaned up the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const indexPage = () => {
         <h1>My name is Isaac, I don't have much yet but I will</h1>
         <h2>I hope I can make some damn money with this</h2>  
         <p>Need a developer for your Web page, front end, back end? 
-          <Link to = "/contact">Contact Me</Link>
+         
         </p>
       
   </Layout>  
